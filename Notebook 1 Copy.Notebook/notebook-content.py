39045e9adcb7df7485013855e04723d31b3ceff8
SaikatLTIM <saikatmukherjee171@gmail.com>
2025-12-02 15:12:20 +0530

    Committing 6 items from workspace a9273c3e-d734-4c73-a916-96aa0f362f98

--- Notebook 1 Copy.Notebook/notebook-content.py
+++ Notebook 1 Copy.Notebook/notebook-content.py
@@ -6,7 +6,18 @@
 # META   "kernel_info": {
 # META     "name": "synapse_pyspark"
 # META   },
-# META   "dependencies": {}
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "146052ad-ed7e-4688-b952-ff5aa7717428",
+# META       "default_lakehouse_name": "Saikat_Learning_LH",
+# META       "default_lakehouse_workspace_id": "682ed0ff-8385-4ba2-ae1d-1cc231862788",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "146052ad-ed7e-4688-b952-ff5aa7717428"
+# META         }
+# META       ]
+# META     }
+# META   }
 # META }
 
 # CELL ********************
@@ -14,6 +25,18 @@
 # Welcome to your new notebook
 # Type here in the cell editor to add code!
 print('Saikat')
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+df = spark.sql("SELECT * FROM Saikat_Learning_LH.table_employee LIMIT 1000")
+display(df)
 
 # METADATA ********************
 
---
+++ Saikat_Warehouse.Warehouse/dbo/Tables/sales_orders.sql
@@ -0,0 +1,9 @@
+CREATE TABLE [dbo].[sales_orders] (
+
+	[OrderID] int NULL, 
+	[EmployeeID] int NULL, 
+	[EmployeeName] varchar(50) NULL, 
+	[Region] varchar(50) NULL, 
+	[OrderDate] date NULL, 
+	[Amount] decimal(10,2) NULL
+);
